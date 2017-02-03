25af9d7b7ba181be5fbe22a0c3ccd07189d7de36
Meredith Nachman <mwnachman@gmail.com>
2017-02-02 17:59:18 -0800

    add break chain and codility practice test

---
+++ breakChain.js
@@ -0,0 +1,79 @@
+// Break the chain into three parts the least expensive way:
+
+// you can't break on the first or last element
+// you can't break on adjacent elements
+// you must break in two spots
+
+// for instance:  [5, 2, 6, 1, 3, 0]
+// you would break on 2 and 3 (because it sums to 5)
+// 1 and 3 would be less, but they are adjacent
+// 1, 2, or 3 and 0 would be less, but you can't break
+// on zero because it's at the end
+
+function solution(A) {
+  var lowestFour = [[Number.MAX_VALUE], [Number.MAX_VALUE], [Number.MAX_VALUE], [Number.MAX_VALUE]];
+  for (var i = 1; i < A.length - 1; i++) {
+    if (A[i] < lowestFour[3][0]) {
+      lowestFour[3] = [A[i], i];
+      lowestFour.sort(function(a, b){return a[0] - b[0]});
+    }    
+  }
+  if (Math.abs(lowestFour[0][1] - lowestFour[1][1]) > 1) {
+    return lowestFour[0][0] + lowestFour[1][0];
+  } else {
+    var firstPair = lowestFour[0][0] + lowestFour[2][0];
+    var secondPair = lowestFour[1][0] + lowestFour[3][0];
+    return firstPair < secondPair ? firstPair : secondPair;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
