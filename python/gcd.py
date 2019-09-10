c075f95de8ff96234aee660b942ca12398cf5ea9
Meredith Nachman <mwnachman@gmail.com>
2019-09-10 18:44:17 -0400

    update GCD in python

--- python/gcd.py
+++ python/gcd.py
@@ -1,22 +1,42 @@
 # Find the greatest common denominator of two numbers
+# gcd(a, b) == gcd(|a|, |b|), so we will restrict ourselves
+# to positive inputs.  
 
 import random
 import timeit
 
-# Efficient
+# Efficient  --  this algorithm is known as Euclid's algorithm
 
 def gcd(higher, lower):
+  # make sure higher and lower really are higher and lower
   if higher < lower:
     higher, lower = lower, higher
+
+  # don't allow division by zero
+  # if either value is zero, return the higher value;
+  # if both are zero, return 0;
+  if lower == 0:
+    return higher
+
+  # get the remainder of the division - if the remainder is 0,
+  # higher was divisible by lower and lower is the largest 
+  # common divisor; otherwise, the remainder becomes the new lower
+  # value (try thinking about it with higher = 147, lower = 42)
   remainder = higher % lower
   if remainder == 0:
     return lower
   return gcd(lower, remainder)
 
-
 # Naive
 
 def gcd_naive(a, b):
+  # if either value is zero, return the higher value;
+  # if both are zero, return 0;
+  if a == 0:
+    return b
+  if b == 0: 
+    return a
+
   current_gcd = 1
   for d in range(2, min(a, b) + 1):
     if a % d == 0 and b % d == 0:
@@ -24,6 +44,7 @@ def gcd_naive(a, b):
         current_gcd = d
 
   return current_gcd
+
 
 
 # Compare the two for correct answers and execution time:
